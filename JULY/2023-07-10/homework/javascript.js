fea8a8d1e5a936e2ff3dfd8eb23b8a6be28f1fbb
Gabrielė Bataitytė <bataitytegabriele991@gmail.com>
2023-07-10 11:55:10 +0300

    first commit

---
+++ JULY/2023-07-10/homework/javascript.js
@@ -0,0 +1,20 @@
+//1 uzduotis
+let vardas = 'Matthew'
+let pavarde = 'McConaughey'
+console.log(vardas)
+
+//2 uzduotis
+let manoVardas = 'Gabrielė'
+let manoPavarde = 'Bataitytė'
+let gimimoMetai = 1999
+let x = 2023 - gimimoMetai
+
+console.log(`Aš esu ${manoVardas} ${manoPavarde}. Man yra ${x} metai`)
+
+//3 uzduotis
+//Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus. Sukurti trečią kintamąjį ir jam priskirti stringą, sudarytą iš trijų paskutinių vardo ir pavardės kintamųjų raidžių. Jį atspausdinti.
+
+vardas = 'Matthew'
+pavarde = 'McConaughey'
+let raides = vardas.slice(-3) + pavarde.slice(-3)
+console.log(raides)

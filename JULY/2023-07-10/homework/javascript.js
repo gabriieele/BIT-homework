02e73be016c78b699ab2d1ff2b99dc19032ad2f4
Gabrielė Bataitytė <bataitytegabriele991@gmail.com>
2023-07-10 12:21:38 +0300

    HW

--- JULY/2023-07-10/homework/javascript.js
+++ JULY/2023-07-10/homework/javascript.js
@@ -16,6 +16,5 @@ console.log(`Aš esu ${manoVardas} ${manoPavarde}. Man yra ${x} metai`)
 
 vardas = 'Matthew'
 pavarde = 'McConaughey'
-// let raides = vardas[4] + vardas[5] + vardas[6] + pavarde[8] + pavarde[9] + pavarde[10]
 let raides = vardas.slice(-3) + pavarde.slice(-3)
 console.log(raides)

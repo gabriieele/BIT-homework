09f8c01f2f8e640a9d63661bfa12d1a1a1316013
Gabrielė Bataitytė <bataitytegabriele991@gmail.com>
2023-07-21 16:39:11 +0300

    kodo issukis 2uzd

--- JULY/2023-07-20/kodo issukis/javascript.js
+++ JULY/2023-07-20/kodo issukis/javascript.js
@@ -34,3 +34,72 @@ for (let i = 0; i < arr.length; i++) {
 }
 console.log(`${rez1} yra arčiausiai ${number}`)
 console.log(`Atsakymas ${rez2} (${skaicius} + ${rez1})`)
+
+//2uzd
+// Kiekvienas mąstytojas savo lentelėje turi keturių atsitiktinių vienaženklių skaitmenų eilę. Kieno pirmasis eilės skaičius didesnis, tas laimi žaidimo raundą, pasiima tiek savąjį, tiek priešininko skaičių ir deda juos į lentelėje esančių skaičių eilės galą mažėjančia tvarka.
+
+// Mąstytojai susiginčijo, kuris laimės žaidimą. Platonokodas įsitikinęs, kad jis bus pirmasis. Padėk jam išsiaiškinti kiek raundų prireiks išlošti visus skaičius iš Aristokodo, jei Platonokodas turi eilę sudarytą iš 8, 1, 4, 7, o Aristokodas eilę iš 2, 5, 3, 6.
+const playerOne = [
+  780, 893, 423, 548, 247, 395, 92, 815, 871, 194, 908, 76, 174, 552, 526, 544, 883, 712, 937, 239,
+  399, 187, 808, 647, 868, 652, 870, 767, 403, 344, 659, 237, 317, 170, 984, 367, 493, 120, 97, 60,
+  235, 615, 117, 684, 337, 157, 200, 905, 725, 1000,
+]
+const playerTwo = [
+  674, 59, 259, 568, 989, 842, 87, 658, 903, 882, 798, 820, 300, 598, 714, 853, 215, 127, 992, 945,
+  971, 748, 789, 455, 394, 404, 113, 364, 962, 649, 356, 324, 796, 709, 627, 562, 904, 313, 374,
+  286, 175, 182, 140, 326, 953, 600, 550, 233, 220, 566,
+]
+
+function rand(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+let newRez = 0
+let newRez1 = 0
+const newArr = []
+const newArr1 = []
+
+for (let i = 0; i < 50; i++) {
+  const index = rand(0, 49)
+  newArr.push(playerOne[index])
+  newArr1.push(playerTwo[index])
+}
+
+while (true) {
+  if (newArr[0] > newArr1[0]) {
+    console.log('Pirmo zaidejo skaiciai:', newArr)
+    console.log('Antro zaidejo skaiciai:', newArr1)
+    console.log(
+      `Žaidėjas nr. 1 žaidžia: ${newArr[0]}, Žaidėjas nr. 2 žaidžia: ${newArr1[0]}. Partiją laimėjo žaidėjas nr. 1!`
+    )
+    newArr.push(newArr.shift())
+    newArr.push(newArr1.shift())
+    const sort = newArr.slice(-2).sort((a, b) => b - a)
+    //grazinimas i masyva, -2 paskutiniai 2, 2 kiek reiksmiu, ...sort kuo pakeista bus
+    newArr.splice(-2, 2, ...sort)
+  } else {
+    console.log('Pirmo zaidejo skaiciai:', newArr)
+    console.log('Antro zaidejo skaiciai:', newArr1)
+    console.log(
+      `Žaidėjas nr. 1 žaidžia: ${newArr[0]}, Žaidėjas nr. 2 žaidžia: ${newArr1[0]}. Pirmą partiją laimėjo žaidėjas nr. 2!`
+    )
+    newArr1.push(newArr1.shift())
+    newArr1.push(newArr.shift())
+    const sort1 = newArr1.slice(-2).sort((a, b) => b - a)
+    //grazinimas i masyva, -2 paskutiniai 2, 2 kiek reiksmiu, ...sort kuo pakeista bus
+    newArr1.splice(-2, 2, ...sort1)
+  }
+
+  if (newArr.length === 1) {
+    console.log(
+      `Žaidėjas nr. 1 žaidžia: ${newArr[0]}, Žaidėjas nr. 2 žaidžia: ${newArr1[0]}, Žaidėjas nr. 1 laimi ŽAIDIMĄ!`
+    )
+    break
+  } else if (newArr1.length === 1) {
+    console.log(
+      `Žaidėjas nr. 1 žaidžia: ${newArr[0]}, Žaidėjas nr. 2 žaidžia: ${newArr1[0]}, Žaidėjas nr. 2 laimi ŽAIDIMĄ!`
+    )
+    break
+  }
+}

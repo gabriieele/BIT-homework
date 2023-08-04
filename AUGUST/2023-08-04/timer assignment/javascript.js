0ca5ddea755ef56a80dfdb4c02498b2e95b388f9
Gabrielė Bataitytė <bataitytegabriele991@gmail.com>
2023-08-04 20:40:26 +0300

    updated

--- AUGUST/2023-08-04/timer assignment/javascript.js
+++ AUGUST/2023-08-04/timer assignment/javascript.js
@@ -1,28 +1,32 @@
 let interval
+const timer = document.getElementById('timer')
+const iputValue = document.getElementById('minutes')
+let time = 0
 let timerRunning = false
-const min = document.getElementById('minutes')
-const sec = document.getElementById('seconds')
-
 function run(e) {
   if (!timerRunning) {
-    let m = parseInt(min.value)
-    let s = parseInt(sec.value)
-    let time = m * 60 + s
+    if (time === 0) {
+      time = iputValue.value * 60
+    }
+    timerRunning = true
     interval = setInterval(() => {
       let minutes = Math.floor(time / 60)
       let seconds = time % 60
       let newTime = `${minutes}m:${seconds.toString().padStart(2, '0')}s`
-      const timer = document.getElementById('timer')
       timer.innerHTML = newTime
       time--
+
       if (time < 0) {
         clearInterval(interval)
         timerRunning = false
+        //document.querySelector('.timer').innerHTML = `Time is over`
+        e.target.textContent = 'Start'
+        e.target.style.backgroundColor = 'rgb(185, 185, 185)'
       }
     }, 1000)
-    e.target.textContent = 'Pause'
-    e.target.style.backgroundColor = 'rgb(235, 235, 235)'
     timerRunning = true
+    e.target.textContent = 'Pause'
+    e.target.style.backgroundColor = 'rgb(235, 235, 235'
   } else {
     clearInterval(interval)
     e.target.textContent = 'Start'
@@ -35,15 +39,11 @@ document.querySelector('.startBtn').onclick = run
 
 function reset() {
   clearInterval(interval)
-  let m = parseInt(min.value)
-  let s = parseInt(sec.value)
-  time = m * 60 + s
-  const startBtn = document.querySelector('.startBtn')
-  startBtn.textContent = 'Start'
-  startBtn.style.backgroundColor = 'rgb(185, 185, 185)'
-  const timer = document.getElementById('timer')
+  timerRunning = false
+  document.querySelector('.startBtn').textContent = 'Start'
+  document.querySelector('.startBtn').style.backgroundColor = 'rgb(185, 185, 185)'
   timer.innerHTML =
-    '<label for="minutes">Minutes:</label>  <input type="number" id="minutes" min="0" value="0" /> <label for="seconds">Seconds:</label>     <input type="number" id="seconds" min="0" value="0" />'
+    '<label for="minutes">Minutes:</label>  <input type="number" id="minutes" min="0" value="0" />'
 }
 
 document.querySelector('.resetBtn').onclick = reset

f6453ff99e9407f67337152a4112f0224abcf20f
Gabrielė Bataitytė <bataitytegabriele991@gmail.com>
2023-08-04 16:03:55 +0300

    correct input

--- AUGUST/2023-08-04/timer assignment/javascript.js
+++ AUGUST/2023-08-04/timer assignment/javascript.js
@@ -1,9 +1,13 @@
-let time = document.querySelector('input').value
 let interval
 let timerRunning = false
+const min = document.getElementById('minutes')
+const sec = document.getElementById('seconds')
 
 function run(e) {
   if (!timerRunning) {
+    let m = parseInt(min.value)
+    let s = parseInt(sec.value)
+    let time = m * 60 + s
     interval = setInterval(() => {
       let minutes = Math.floor(time / 60)
       let seconds = time % 60
@@ -11,7 +15,6 @@ function run(e) {
       const timer = document.getElementById('timer')
       timer.innerHTML = newTime
       time--
-
       if (time < 0) {
         clearInterval(interval)
         timerRunning = false
@@ -32,12 +35,15 @@ document.querySelector('.startBtn').onclick = run
 
 function reset() {
   clearInterval(interval)
-  time = 600
+  let m = parseInt(min.value)
+  let s = parseInt(sec.value)
+  time = m * 60 + s
   const startBtn = document.querySelector('.startBtn')
   startBtn.textContent = 'Start'
   startBtn.style.backgroundColor = 'rgb(185, 185, 185)'
   const timer = document.getElementById('timer')
-  timer.innerHTML = '10m:00s'
+  timer.innerHTML =
+    '<label for="minutes">Minutes:</label>  <input type="number" id="minutes" min="0" value="0" /> <label for="seconds">Seconds:</label>     <input type="number" id="seconds" min="0" value="0" />'
 }
 
 document.querySelector('.resetBtn').onclick = reset

06c459f47d00f4b587502988857be35b60bf1c06
Gabrielė Bataitytė <bataitytegabriele991@gmail.com>
2023-08-14 17:43:44 +0300

    game

--- AUGUST/2023-08-01/game/javascript.js
+++ AUGUST/2023-08-01/game/javascript.js
@@ -11,65 +11,107 @@ function startGame(e) {
   player.style = 'display:block'
   document.querySelector('h1').style = 'display:none'
   const mainPart = document.querySelector('.bckgrnd')
-
+  const pointsShow = document.querySelector('.computer')
+  const winner = document.querySelector('.winner')
+  const playerPointsPlace = document.querySelector('.player')
   let isGoing = false
   let time = 30
+  let round = 1
+  let computerRounds = 0
+  let playerRounds = 0
   let interval
+  let isClicked = false
 
   function rand(min, max) {
     min = Math.ceil(min)
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
-  //   player.addEventListener('click', function () {
-  //     player.style.backgroundColor = 'blue'
-  //   })
 
-  //   const click = document.querySelector('.square').addEventListener('click', function () {
-  //     player.style.backgroundColor = `rgb(${rand(0, 255)}, ${rand(0, 255)}, ${rand(0, 255)})`
-  //     const newTop = rand(0, mainPart.innerHeight - player.clientHeight)
-  //     const newLeft = rand(0, mainPart.innerWidth - player.clientWidth)
+  function rounds() {
+    isClicked = false
+    pointsShow.innerHTML = `Computer Points: `
+    playerPointsPlace.innerHTML = `Player Points: `
+    if (round <= 10) {
+      if (!isGoing) {
+        interval = setInterval(() => {
+          player.style.backgroundColor = `rgb(${rand(0, 255)}, ${rand(0, 255)}, ${rand(0, 255)})`
+          const newTop = rand(0, mainPart.clientHeight - player.clientHeight)
+          const newLeft = rand(0, mainPart.clientWidth - player.clientWidth)
 
-  //     // Set the new position of the player element
-  //     player.style.position = 'absolute'
-  //     player.style.top = `${newTop}px`
-  //     player.style.left = `${newLeft}px`
-  //   })
+          player.style.position = 'absolute'
+          player.style.top = `${newTop}px`
+          player.style.left = `${newLeft}px`
+          let minutes = Math.floor(time / 60)
+          let seconds = time % 60
+          let newTime = `${minutes}:${seconds.toString().padStart(2, '0')}`
+          const timer = document.getElementById('timer')
+          timer.innerHTML = newTime
+          time--
 
-  if (!isGoing)
-    interval = setInterval(() => {
-      player.style.backgroundColor = `rgb(${rand(0, 255)}, ${rand(0, 255)}, ${rand(0, 255)})`
-      const newTop = rand(0, mainPart.clientHeight - player.clientHeight)
-      const newLeft = rand(0, mainPart.clientWidth - player.clientWidth)
+          if (!isClicked) {
+            computerPoints++
+            isClicked = false
+            pointsShow.innerHTML = `Computer Points: ${computerPoints}`
+          }
 
-      player.style.position = 'absolute'
-      player.style.top = `${newTop}px`
-      player.style.left = `${newLeft}px`
-      let minutes = Math.floor(time / 60)
-      let seconds = time % 60
-      let newTime = `${minutes}:${seconds.toString().padStart(2, '0')}`
-      const timer = document.getElementById('timer')
-      timer.innerHTML = newTime
-      time--
-      if (time < 0) {
-        clearInterval(interval)
+          if (time < 0) {
+            clearInterval(interval)
+            if (!isClicked) {
+              if (computerPoints > points) {
+                computerRounds++
+                winner.innerHTML = `Computer won the round with ${computerPoints} points. Computer won ${computerRounds} rounds, you won ${playerRounds} rounds`
+              } else if (points > computerPoints) {
+                playerRounds++
+                winner.innerHTML = `You won the round with ${points} points. You won ${playerRounds} rounds, computer won ${computerRounds} rounds`
+              } else if (points === computerPoints) {
+                playerRounds++
+                computerRounds++
+                winner.innerHTML = `It's a tie! You won ${playerRounds} rounds, computer won ${computerRounds} rounds`
+              }
+              points = 0
+              computerPoints = 0
+              time = 30
+              round++
+              rounds()
+              if (round === 11) {
+                if (playerRounds > computerRounds) {
+                  winner.innerHTML += `<br>You won the game!`
+                } else {
+                  winner.innerHTML += `<br>Game over! Computer won`
+                }
+                e.target.style = 'display:block'
+                player.style = 'display:none'
+                document.querySelector('h1').style = 'display:block'
+                winner.innerHTML = ''
+              }
+            }
+          }
+        }, 1000)
       }
-    }, 1000)
-}
-let points = 0
-function pointsSum() {
-  let rez = ''
-  points++
-  rez += points + ''
-  console.log(`Taškai: ${rez}`)
-  // Add your code here to perform actions when the button is clicked
+    }
+  }
+  rounds()
 }
 
-// Add an event listener to the button
-player = document.addEventListener('click', pointsSum)
+let isClicked = false
+let computerPoints = 0
+let points = 0
+
+function pointsSum() {
+  if (!isClicked) {
+    points++
+    const playerPointsPlace = document.querySelector('.player')
+    playerPointsPlace.innerHTML = `Your Points: ${points}`
+    isClicked = true
+
+    setTimeout(() => {
+      isClicked = false
+    }, 1000)
+  }
+}
 
 document.querySelector('.btn').onclick = startGame
-
 // Sukurkite žaidimą.
 // Žaidimą žaidžia du žaidėjai: Player(Jūs) ir Kompiuteris.
 // Žaidimą sudaro 10 raundų, vieno raundo trukmė 30 sekundžių.

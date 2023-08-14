b308ce269ba8d0dce3e2ee3732def598ce449b23
Gabrielė Bataitytė <bataitytegabriele991@gmail.com>
2023-08-14 12:36:46 +0300

    react

---
+++ AUGUST/2023-08-14/index.js
@@ -0,0 +1,24 @@
+//console.log('labas pasauli')
+
+// const x = 10
+// const arr = [1, 2, 3]
+// console.log('veikia')
+
+// import * as viskas from './functions.js'
+// console.log(viskas)
+
+// import funkcija from './functions.js' //galime bet koki varda priskirti, kai yra default reiksme
+// console.log(funkcija(10, 2))
+
+// import { sum } from './functions.js'
+
+// import { sum as sum2 } from './math.js'
+
+// import m, { deduct } from './math.js'
+// console.log(m(10, 25))
+// console.log(deduct(10, 25))
+// console.log(sum(10, 2))
+// console.log(sum2(15, 22))
+
+import chalk from 'chalk'
+console.log(chalk.blue('hello world'))
---
+++ AUGUST/2023-08-14/math.js
@@ -0,0 +1,20 @@
+export const sum = (a, b) => {
+  return 'Math.js' + a + b
+}
+
+export const deduct = (a, b) => {
+  return 'Math.js' + a - b
+}
+
+export const divide = (a, b) => {
+  return 'Math.js' + a / b
+}
+
+export const multiply = (a, b) => {
+  return 'Math.js' + a * b
+}
+
+//eksportuojama duomenu pervadinimas
+// export { multiply as m }
+
+// export default m

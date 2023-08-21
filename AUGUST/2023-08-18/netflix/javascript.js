b98d12f580f3373f91112f2d739a3f55bbcf9d2b
Gabrielė Bataitytė <bataitytegabriele991@gmail.com>
2023-08-21 20:24:38 +0300

    fix

--- AUGUST/2023-08-18/netflix/javascript.js
+++ AUGUST/2023-08-18/netflix/javascript.js
@@ -1,32 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const faqItems = document.querySelectorAll('.question')
+const faqItems = document.querySelectorAll('.question')
 
-  faqItems.forEach(item => {
-    item.addEventListener('click', () => {
-      item.classList.toggle('active')
-    })
+faqItems.forEach(item => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('active')
   })
+})
 
-  const signInButton = document.getElementById('signInButton')
-  const signInModal = new bootstrap.Modal(document.getElementById('exampleModal'))
-  const header = document.querySelector('header')
-  const footer = document.querySelector('footer')
-  const sections = document.querySelectorAll('section')
+const signInButton = document.getElementById('signInButton')
+const signInModal = new bootstrap.Modal(document.getElementById('exampleModal'))
+const header = document.querySelector('header')
+const footer = document.querySelector('footer')
+const sections = document.querySelectorAll('section')
 
-  signInButton.addEventListener('click', () => {
-    signInModal.show()
-    sections.forEach(section => {
-      section.style.display = 'none'
-    })
-    header.style.display = 'none'
-    footer.style.display = 'none'
+signInButton.addEventListener('click', () => {
+  signInModal.show()
+  sections.forEach(section => {
+    section.style.display = 'none'
   })
-
-  signInModal.addEventListener('click', () => {
-    sections.forEach(section => {
-      section.style.display = 'block'
-    })
-    header.style.display = 'block'
-    footer.style.display = 'block'
-  })
+  header.style.display = 'none'
+  footer.style.display = 'none'
 })

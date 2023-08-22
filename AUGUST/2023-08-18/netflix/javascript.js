84f0659c5fac21eb6e0fc83831ae2738c5755254
Gabrielė Bataitytė <bataitytegabriele991@gmail.com>
2023-08-22 13:32:42 +0300

    new features addes

--- AUGUST/2023-08-18/netflix/javascript.js
+++ AUGUST/2023-08-18/netflix/javascript.js
@@ -1,5 +1,20 @@
+function selectLanguage(e, language) {
+  e.preventDefault()
+  document.getElementById('selected').textContent = language
+  document.getElementById('selected1').textContent = language
+
+  console.log(language)
+}
+
+const icons = document.querySelectorAll('.icons')
+
+icons.forEach(icon => {
+  icon.innerHTML +=
+    '<span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16"><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg></span>'
+})
+
 const faqItems = document.querySelectorAll('.question')
-let isClicked = false
+
 faqItems.forEach(item => {
   item.addEventListener('click', () => {
     item.classList.toggle('active')
@@ -22,12 +37,12 @@ signInButton.addEventListener('click', () => {
   footer.style.display = 'none'
 })
 
-signInModal.addEventListener('hidden.bs.modal', () => {
-  if (isClicked === false) {
-    sections.forEach(section => {
-      section.style.display = 'block'
-    })
-    header.style.display = 'block'
-    footer.style.display = 'block'
-  }
-})
+// signInModal.addEventListener('hidden.bs.modal', () => {
+//   if (isClicked === false) {
+//     sections.forEach(section => {
+//       section.style.display = 'block'
+//     })
+//     header.style.display = 'block'
+//     footer.style.display = 'block'
+//   }
+// })

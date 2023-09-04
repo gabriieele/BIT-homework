59bc0d80effed6a09bec91569d1661f711c4ecee
Gabrielė Bataitytė <bataitytegabriele991@gmail.com>
2023-09-04 18:01:19 +0300

    homework

---
+++ SEPTEMBER/file-manager/index.php
@@ -0,0 +1,118 @@
+<?php
+//nuorodu masyvas
+$directoryPaths = [
+    'audio' => 'directories/audio',
+    'demo' => 'directories/demo',
+    'embed' => 'directories/embed',
+    'images' => 'directories/images',
+    'video' => 'directories/video',
+];
+//vardai failu, kad nerodytu audio.php ir tt
+$directoryNames = [
+    'audio' => 'Audio',
+    'demo' => 'Demo',
+    'embed' => 'Embed',
+    'images' => 'Images',
+    'video' => 'Video',
+];
+
+$home = './index.php';
+
+//failai kataloguose
+function filterContents($dir) {
+    //scnadir - visas sarasas su failais ir kategorijom 
+    $files = scandir($dir);
+    $fileList = [];
+
+    foreach ($files as $file) {
+        //Jei nera nei ., nei .. kategorijoj, tada pridedamas prie tuscio amsyvo (pasalinama, nes nereikia atvaizduot)
+        if ($file !== '.' && $file !== '..') {
+            $fileList[] = $file;
+        }
+    }
+    return $fileList;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File manager</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+    <nav class="navbar navbar-expand-lg justify-content-between">
+  <div class="container-fluid">
+    <div class="logo"><a class="navbar-brand" href="#">H3K DEMO</a>
+    <a class="navbar-brand" href=<?= $home; ?>><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+  <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z"/>
+</svg></a></div>
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+    <div class="input-group">
+  <input type="text" class="form-control" aria-label="Text input with dropdown button">
+  <button class="btn btn-outline-secondary dropdown-toggle d-flex align-items-center" type="button" data-bs-toggle="dropdown" aria-expanded="false"></button>
+
+</div>
+    
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#"><svg class="me-1" style="color: grey" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-arrow-up" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M7.646 5.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2z"/>
+  <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
+</svg>Upload</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><svg class="me-1" style="color: grey" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+</svg>New Item</a>
+        </li>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><svg class="me-1" style="color: grey" width="16" height="16" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+  <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+</svg>Settings</a>
+        </li>
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+    </header>
+    <div class="container"><table class="table table-striped mt-4">   <thead>
+                    <tr>
+                        <th scope="col"><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Size</th>
+                        <th scope="col">Modified</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead> <tbody>
+        <?php foreach ($directoryPaths as $directoryName => $directoryPath) : ?>
+          
+               
+                    <?php
+                    $directoryList = filterContents($directoryPath);
+                    foreach ($directoryList as $file) :
+                    ?>
+                        <tr>
+                            <th scope="row"><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></th>
+                            <td><a href="<?= $directoryPath . '/' . $file; ?>"><?= $directoryNames[$directoryName]; ?></a></td>
+                            <!-- //is_dir patikrina ar failas ar folderis -->
+                            <td><?= is_dir($directoryPath . '/' . $file) ? 'Folder' : 'File'; ?></td>
+                            <td><?= date('Y-m-d H:i', filemtime($directoryPath . '/' . $file)); ?></td>
+                            <td>Actions</td>
+                        </tr>
+                       
+                    <?php endforeach; ?>
+               
+            
+        <?php endforeach; ?> </tbody></table>
+    </div>
+</body>
+</html>

73fcae1491ee91afc11fb8a35048a3d127388a40
Gabrielė Bataitytė <bataitytegabriele991@gmail.com>
2023-09-06 18:04:09 +0300

    icons and rename

--- SEPTEMBER/file-manager/index.php
+++ SEPTEMBER/file-manager/index.php
@@ -2,9 +2,7 @@
 $path = isset($_GET['path']) ? $_GET['path'] : '.';
 
 $files = scandir($path);
-
 unset($files[0]);
-
 if($path === '.'){
   unset($files[1]);
 }
@@ -44,6 +42,7 @@ if (isset($_FILES['failas']) && $_FILES['failas']['size'] > 0) {
         } elseif ($_POST['createType'] === 'file') {
             $filePath = $path . '/' . $_POST['itemName'];
             //naujas failas
+            //failo sukūrimui naudojama touch
             if (!touch($filePath)) {
                 echo 'Klaida kuriant faila';
             } else {
@@ -55,7 +54,38 @@ if (isset($_FILES['failas']) && $_FILES['failas']['size'] > 0) {
     }
 }
 
-    
+if (isset($_POST['newItem']) && isset($_POST['oldFIleName'])) {
+  $newName = $_POST['newItem'];
+  $oldName = $_POST['oldFIleName'];
+
+   //ar pasirinktas failas egzistuoja
+  if (file_exists($path . '/' . $oldName)) {
+      $oldPath = $path . '/' . $oldName;
+      $newPath = $path . '/' . $newName;
+
+//ar jau nera tokio pat failo pavadinimo
+      if (file_exists($newPath)) {
+        echo "Toks pavadinimas jau egzistuoja";
+      } else {
+         //pervadinimas failu
+          if (rename($oldPath, $newPath)) {
+           
+            echo "Failas pervadintas sekmingai";
+    //duomenu atnaujinimas pasikeitus failu pavadinimams, nes filesize gauna errora, nes vis dar tikrina sena faila
+    $files = scandir($path);
+    unset($files[0]);
+    if($path === '.'){
+      unset($files[1]);
+    }
+           
+          } else {
+              echo "Klaida pervadinant faila";
+          }
+      }
+  } 
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -63,6 +93,7 @@ if (isset($_FILES['failas']) && $_FILES['failas']['size'] > 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>File manager</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
@@ -85,22 +116,14 @@ if (isset($_FILES['failas']) && $_FILES['failas']['size'] > 0) {
     
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link upld" aria-current="page" href="#"><svg class="me-1" style="color: grey" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-arrow-up" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M7.646 5.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2z"/>
-  <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
-</svg>Upload</a>
+          <a class="nav-link upld" aria-current="page" href="#"><i class="bi bi-cloud-arrow-up-fill ms-1 me-1"></i>Upload</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link newItem" href="#"><svg class="me-1" style="color: grey" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
-  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-</svg>New Item</a>
+          <a class="nav-link newItem" href="#"><i class="bi bi-plus-square me-1"></i>New Item</a>
         </li>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"><svg class="me-1" style="color: grey" width="16" height="16" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
-  <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
-</svg>Settings</a>
+          <a class="nav-link" href="#"><i class="bi bi-gear-fill me-1"></i>Settings</a>
         </li>
       </ul>
      
@@ -108,7 +131,17 @@ if (isset($_FILES['failas']) && $_FILES['failas']['size'] > 0) {
   </div>
 </nav>
     </header>
-    <div class="container"><table class="table table-striped mt-4">   <thead>
+    <div class="container">
+      
+
+    
+        <form method="POST" class="editName inline-form">
+          
+        </form>
+
+
+    <table class="table table-striped mt-4">   
+      <thead>
                     <tr>
                         <th scope="col"><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></th>
                         <th scope="col">Name</th>
@@ -116,49 +149,105 @@ if (isset($_FILES['failas']) && $_FILES['failas']['size'] > 0) {
                         <th scope="col">Modified</th>
                         <th scope="col">Actions</th>
                     </tr>
+                    
                 </thead> <tbody>
-        
-          
-               
+   
                     <?php
                 
                     foreach ($files as $file) {
                       if($file !== "index.php" and $file !== "style.css"){
+                        //tikrinant ar tai failas ar folderis, reikia visa kelia nurodyt, kitu atveju subdirektorijose neskaiciuos faily dydziu
+                        $filePath = $path . '/' . $file;
+                        //failp visai info, is ten bus imamamas parametras extension
+                        $fileExt = pathinfo($file);
+                        $fileWithIcon = $file;
+         
+
+                        if (is_dir($filePath)) {
+                          $fileWithIcon = '<i class="bi bi-folder"></i> ' . $file;
+                        }
+                        else if (array_key_exists('extension', $fileExt)) {
+                          
+                        if ($fileExt['extension'] === 'png' OR $fileExt['extension'] === 'jpeg') {
+                          $fileWithIcon = '<i class="bi bi-image"></i> ' . $file;
+                        }
+                        if ($fileExt['extension'] === 'php') {
+                          $fileWithIcon = '<i class="bi bi-filetype-php"></i> ' . $file;
+                        }
+                        if ($fileExt['extension'] === 'pdf') {
+                          $fileWithIcon = '<i class="bi bi-file-pdf"></i> ' . $file;
+                        }
+                        if ($fileExt['extension'] === 'doc' or $fileExt['extension'] === 'docx') {
+                          $fileWithIcon = '<i class="bi bi-file-earmark-word"></i> ' . $file;
+                        }
+                        if ($fileExt['extension'] === 'txt') {
+                          $fileWithIcon = '<i class="bi bi-body-text"></i> ' . $file;
+                        }
+                        if ($fileExt['extension'] === 'mp3') {
+                          $fileWithIcon = '<i class="bi bi-music-note-beamed"></i> ' . $file;
+                        }
+                        if ($fileExt['extension'] === 'mp4' or $fileExt['extension'] === 'avi') {
+                          $fileWithIcon = '<i class="bi bi-film"></i> ' . $file;
+                        }
+                        if ($fileExt['extension'] === 'html') {
+                          $fileWithIcon = '<i class="bi bi-filetype-html"></i> ' . $file;
+                        }
+                        if ($fileExt['extension'] === 'css') {
+                          $fileWithIcon = '<i class="bi bi-filetype-css"></i> ' . $file;
+                        }
+                        if ($fileExt['extension'] === 'js') {
+                          $fileWithIcon = '<i class="bi bi-filetype-js"></i> ' . $file;
+                        }
+                       
+                        }
+                      else {
+                 $fileWithIcon = '<i class="bi bi-file-earmark"></i>' . $file; 
+                      }
+
                       
                     ?>
+                    
                         <tr>
                             <th scope="row"><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></th>
-
                             <td>
                               <!-- dirname grazins tevinio folderio pavadinima -->
                               <!-- Jei $file yra '..', tai nuoroda ves i tevini folderi, naudojant dirname($path) funkcija (vienu lygiu auksciau)
                        kitu atveju, jei $file nėra '..', tai direktorija bus dabartinis kelias $path, pridedant $file su / -->
-                              <a href="?path=<?= $file === '..' ? dirname($path) : ($file === '..' ? '' : $path . '/' . $file) ?>">
-                              <?= $file === '..' ? '..' : $file ?></a>
+                       <a href="?path=<?= $file === '..' ? dirname($path) : ($file === '..' ? '' : $path . '/' . $file) ?>">
+                    <?= $file === '..' ? '..' : $fileWithIcon ?>
+                </a>
+      
+            </td>
+          
+                   <td><?= is_dir($filePath) ? 'Folder' : round((filesize($filePath) / 1000),2) . ' KB' ?></td>
+                            <td><?= date('Y-m-d H:i', filemtime($filePath)); ?></td>
+                            <td>
+                              <a href="#" class="edit" data-file="<?= $file ?>"><i class="bi bi-pencil-square"></i></a>
+                              <a href="#"><i class="bi bi-trash-fill ms-1"></i></a>
                             </td>
-                            <td>Folder</td>
-                            <td><?= date('Y-m-d H:i', filemtime($path . '/' . $file)); ?></td>
-                            <td>Actions</td>
                         </tr>
+
                         <?php
+                       
                        }
                       }
                     ?> 
-               
+                                       
             
          </tbody></table>
-         <form class="upload" method="POST" enctype="multipart/form-data">
+         <form class="upload" method="POST" enctype="multipart/form-data"></form>
+         <form class="create" method="POST" enctype="multipart/form-data"></form>
       
-    </form>
-    <form class="create mt-3" method="POST">
+    
 
-</form>
     </div>
     <script>
 const upload = document.querySelector(".upload");
 const create = document.querySelector(".create");
 const btn = document.querySelector(".newItem");
 const btn2 = document.querySelector(".upld");
+const edit = document.querySelectorAll(".edit");
+const editName = document.querySelector(".editName");
 
 btn.addEventListener('click', (e) => {
   e.preventDefault();
@@ -168,24 +257,33 @@ btn.addEventListener('click', (e) => {
   '<input type="radio" class="form-check-input" id="createDirectory" name="createType" value="directory">' +
   '<label class="form-check-label" for="createDirectory">Folder</label>' +
   '</div>' +
-  '<div class="mb-3 form-check">' +
+  '<div class="mb-2 form-check">' +
   '<input type="radio" class="form-check-input" id="createFile" name="createType" value="file">' +
   '<label class="form-check-label" for="createFile">File</label>' +
   '</div>' +
-  '<div class="buttons mt-3">' +
+  '<div class="buttons">' +
   '<button class="btn btn-primary" type="submit" name="createItem">Create</button>' +
   '</div>'
 
 });
 btn2.addEventListener('click', (e) => {
   e.preventDefault();
-  upload.innerHTML = ' <input type="file" class="form-control" name="failas"><div class="buttons mt-3"><button class="btn btn-primary">Upload</button> <button class=" hide btn btn-primary ms-2">Hide</button></div>'
+  upload.innerHTML = ' <input type="file" class="form-control" name="failas"><div class="buttons mt-3 mb-3"><button class="btn btn-primary">Upload</button> <button class=" hide btn btn-primary ms-2">Hide</button></div>'
+});
+edit.forEach(editButton => {
+  editButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const fileName = editButton.getAttribute('data-file');
+    editName.innerHTML = `<div class="mt-3 mb-2"><label>Rename file:</label></div><input type="text" name="newItem" class="form-control"  value="${fileName}"><input type="hidden" name="oldFIleName" value="${fileName}"><div class="buttons mt-3"><button type="submit" class="btn btn-small btn-primary">Rename</button></div>`;
+  });
 });
 
-const hide = document.querySelector(".hide")
+const hide = document.querySelectorAll(".hide")
 
-hide.addEventListener('click', () => {
-upload.innerHTML = ''
+hide.forEach(hideBtn => {
+  hideBtn.addEventListener('click', () => {
+    upload.innerHTML = '';
+  });
 });
 
      
